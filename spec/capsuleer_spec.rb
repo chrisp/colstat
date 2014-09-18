d28517c6cc9ea358f41ca61e8d169473de828796
chrisp <chrisp21@gmail.com>
2014-09-18 15:33:58 -0500

    move requst stubs to global before

--- spec/capsuleer_spec.rb
+++ spec/capsuleer_spec.rb
@@ -5,49 +5,6 @@ require "#{specdir}/spec_helper"
 describe Capsuleer do
   describe '#new' do
     it 'should initialize colony data for passed api data' do
-      keys = YAML.load_file('keys.yml.example')
-
-      response = File.read('spec/xml/planetary_colonies.xml')
-      base_url = 'https://api.eveonline.com'
-      path = 'char/PlanetaryColonies.xml.aspx'
-      stub_request(:get, 
-        "#{base_url}/#{path}?characterID=#{keys[0]['id']}&keyID=#{keys[0]['key_id']}" + 
-        "&vCode=#{keys[0]['vcode']}").
-          to_return(
-            :status => 200, 
-            :body => response, 
-            :headers => {'Content-Type' => 'application/xml; charset=utf-8'})
-
-
-      ['95255214', '95255216', '95255225', '95255229', '95255238'].each do |col_id|
-        response = File.read("spec/xml/pins#{col_id}.xml")
-        path = 'char/PlanetaryPins.xml.aspx'
-        stub_request(:get, "#{base_url}/#{path}?characterID=#{keys[0]['id']}" + 
-          "&keyID=#{keys[0]['key_id']}&planetID=#{col_id}&vCode=#{keys[0]['vcode']}").
-            to_return(
-              :status => 200, 
-              :body => response, 
-              :headers => {'Content-Type' => 'application/xml; charset=utf-8'})
-
-        response = File.read("spec/xml/links#{col_id}.xml")
-        path = 'char/PlanetaryLinks.xml.aspx'
-        stub_request(:get, "#{base_url}/#{path}?characterID=#{keys[0]['id']}" + 
-          "&keyID=#{keys[0]['key_id']}&planetID=#{col_id}&vCode=#{keys[0]['vcode']}").
-            to_return(
-              :status => 200, 
-              :body => response, 
-              :headers => {'Content-Type' => 'application/xml; charset=utf-8'})
-
-        response = File.read("spec/xml/routes#{col_id}.xml")
-        path = 'char/PlanetaryRoutes.xml.aspx'
-        stub_request(:get, "#{base_url}/#{path}?characterID=#{keys[0]['id']}" + 
-          "&keyID=#{keys[0]['key_id']}&planetID=#{col_id}&vCode=#{keys[0]['vcode']}").
-            to_return(
-              :status => 200, 
-              :body => response, 
-              :headers => {'Content-Type' => 'application/xml; charset=utf-8'})
-      end
-
       eve_api = EveApi.new(keys[0]['id'], keys[0]['key_id'], keys[0]['vcode'])
       capsuleer = Capsuleer.new(eve_api)
       expect(capsuleer.colonies).to_not be_nil
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 libdir = File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib'))
+specdir = File.expand_path(File.dirname(__FILE__))
 require 'crack'
 require 'httparty'
 require "#{libdir}/eve_api"
@@ -7,8 +8,54 @@ require "#{libdir}/colony"
 require 'pry'
 require 'webmock/rspec'
 
-# class EveApi
-#   def self.get(url)
-#     puts url
-#   end
-# end
+RSpec.configure do |config|
+  config.before(:each) do
+    @keys = YAML.load_file('keys.yml.example')
+    def keys; @keys; end
+    @eve_api = EveApi.new(keys[0]['id'], keys[0]['key_id'], keys[0]['vcode'])
+    def eve_api; @eve_api; end
+
+    
+    response = File.read('spec/xml/planetary_colonies.xml')
+    base_url = 'https://api.eveonline.com'
+    path = 'char/PlanetaryColonies.xml.aspx'
+    stub_request(:get, 
+                 "#{base_url}/#{path}?characterID=#{keys[0]['id']}&keyID=#{keys[0]['key_id']}" + 
+                 "&vCode=#{keys[0]['vcode']}").
+      to_return(
+                :status => 200, 
+                :body => response, 
+                :headers => {'Content-Type' => 'application/xml; charset=utf-8'})
+
+
+    ['95255214', '95255216', '95255225', '95255229', '95255238'].each do |col_id|
+      response = File.read("spec/xml/pins#{col_id}.xml")
+      path = 'char/PlanetaryPins.xml.aspx'
+      stub_request(:get, "#{base_url}/#{path}?characterID=#{keys[0]['id']}" + 
+                   "&keyID=#{keys[0]['key_id']}&planetID=#{col_id}&vCode=#{keys[0]['vcode']}").
+        to_return(
+                  :status => 200, 
+                  :body => response, 
+                  :headers => {'Content-Type' => 'application/xml; charset=utf-8'})
+
+      response = File.read("spec/xml/links#{col_id}.xml")
+      path = 'char/PlanetaryLinks.xml.aspx'
+      stub_request(:get, "#{base_url}/#{path}?characterID=#{keys[0]['id']}" + 
+                   "&keyID=#{keys[0]['key_id']}&planetID=#{col_id}&vCode=#{keys[0]['vcode']}").
+        to_return(
+                  :status => 200, 
+                  :body => response, 
+                  :headers => {'Content-Type' => 'application/xml; charset=utf-8'})
+
+      response = File.read("spec/xml/routes#{col_id}.xml")
+      path = 'char/PlanetaryRoutes.xml.aspx'
+      stub_request(:get, "#{base_url}/#{path}?characterID=#{keys[0]['id']}" + 
+                   "&keyID=#{keys[0]['key_id']}&planetID=#{col_id}&vCode=#{keys[0]['vcode']}").
+        to_return(
+                  :status => 200, 
+                  :body => response, 
+                  :headers => {'Content-Type' => 'application/xml; charset=utf-8'})
+
+    end
+  end
+end
