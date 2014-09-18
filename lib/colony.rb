ff4e4e6064774b6ddb93a6c29aeb808d7c7bb89a
chrisp <chrisp21@gmail.com>
2014-09-18 14:38:43 -0500

    stubs but no data for pins/links/route specs

--- lib/colony.rb
+++ lib/colony.rb
@@ -12,19 +12,19 @@ class Colony
     self.eve_api = new_eve_api
 
     base_url = "https://api.eveonline.com"
-    path = "/char/PlanetaryPins.xml.aspx"
+    path = "char/PlanetaryPins.xml.aspx"
     self.url = "#{base_url}/#{path}?characterID=#{eve_api.capsuleer_id}" + 
       "&keyID=#{eve_api.key_id}&vCode=#{eve_api.vcode}&planetID=#{id}"
     self.response = EveApi.get(url)
     self.pin_data = response["eveapi"]["result"]["rowset"]["row"]
 
-    path = '/char/PlanetaryLinks.xml.aspx'
+    path = 'char/PlanetaryLinks.xml.aspx'
     self.url = "#{base_url}/#{path}?characterID=#{eve_api.capsuleer_id}" + 
       "&keyID=#{eve_api.key_id}&vCode=#{eve_api.vcode}&planetID=#{id}"
     self.response = EveApi.get(url)
     self.link_data = response["eveapi"]["result"]["rowset"]["row"]
 
-    path = '/char/PlanetaryRoutes.xml.aspx'
+    path = 'char/PlanetaryRoutes.xml.aspx'
     self.url = "#{base_url}/#{path}?characterID=#{eve_api.capsuleer_id}" +
       "&keyID=#{eve_api.key_id}&vCode=#{eve_api.vcode}&planetID=#{id}"
     self.response = EveApi.get(url)
--- spec/capsuleer_spec.rb
+++ spec/capsuleer_spec.rb
@@ -8,14 +8,45 @@ describe Capsuleer do
       keys = YAML.load_file('keys.yml.example')
 
       response = File.read('spec/xml/planetary_colonies.xml')
-      base_path = 'https://api.eveonline.com/char/PlanetaryColonies.xml.aspx'
+      base_url = 'https://api.eveonline.com'
+      path = 'char/PlanetaryColonies.xml.aspx'
       stub_request(:get, 
-        "#{base_path}?characterID=#{keys[0]['id']}&keyID=#{keys[0]['key_id']}" + 
+        "#{base_url}/#{path}?characterID=#{keys[0]['id']}&keyID=#{keys[0]['key_id']}" + 
         "&vCode=#{keys[0]['vcode']}").
           to_return(
             :status => 200, 
             :body => response, 
             :headers => {'Content-Type' => 'application/xml; charset=utf-8'})
+
+
+      # TODO need at least 1 per pin/link/route
+      response = File.read('spec/xml/pins95255214.xml')
+
+      ['95255214', '95255216', '95255225', '95255229', '95255238'].each do |col_id|
+        path = 'char/PlanetaryPins.xml.aspx'
+        stub_request(:get, "#{base_url}/#{path}?characterID=#{keys[0]['id']}" + 
+          "&keyID=#{keys[0]['key_id']}&planetID=#{col_id}&vCode=#{keys[0]['vcode']}").
+            to_return(
+              :status => 200, 
+              :body => response, 
+              :headers => {'Content-Type' => 'application/xml; charset=utf-8'})
+
+        path = 'char/PlanetaryLinks.xml.aspx'
+        stub_request(:get, "#{base_url}/#{path}?characterID=#{keys[0]['id']}" + 
+          "&keyID=#{keys[0]['key_id']}&planetID=#{col_id}&vCode=#{keys[0]['vcode']}").
+            to_return(
+              :status => 200, 
+              :body => response, 
+              :headers => {'Content-Type' => 'application/xml; charset=utf-8'})
+
+        path = 'char/PlanetaryRoutes.xml.aspx'
+        stub_request(:get, "#{base_url}/#{path}?characterID=#{keys[0]['id']}" + 
+          "&keyID=#{keys[0]['key_id']}&planetID=#{col_id}&vCode=#{keys[0]['vcode']}").
+            to_return(
+              :status => 200, 
+              :body => response, 
+              :headers => {'Content-Type' => 'application/xml; charset=utf-8'})
+      end
 
       eve_api = EveApi.new(keys[0]['id'], keys[0]['key_id'], keys[0]['vcode'])
       capsuleer = Capsuleer.new(eve_api)
