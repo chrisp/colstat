c23718319774b931f726ad1696bd39477716c218
chrisp <chrisp21@gmail.com>
2014-09-19 10:48:29 -0500

    add sqlite3 for eve db

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 
 gem 'httparty'
 gem 'crack'
+gem 'bundle'
+gem 'sqlite3'
 
 # test
 gem 'rspec'
--- colstat.rb
+++ colstat.rb
@@ -5,12 +5,15 @@ require 'httparty'
 require 'pp'
 require 'yaml'
 require 'pry'
+require "sqlite3"
 
 libdir = File.expand_path(File.join(File.dirname(__FILE__), 'lib'))
 require "#{libdir}/eve_api"
+require "#{libdir}/eve_db"
 require "#{libdir}/colony"
 require "#{libdir}/capsuleer"
 require "#{libdir}/report"
+
 
 def run_report
   report = Report.new
