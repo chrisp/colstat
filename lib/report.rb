2be76d3ea606ea6cd6a5cf5ddf5fd3a3207d765a
chrisp <chrisp21@gmail.com>
2015-05-29 12:45:21 -0500

    refactors/cleanup for reports

--- lib/report.rb
+++ lib/report.rb
@@ -28,7 +28,7 @@ class Report
     @inputs = Hash.new(0)
   end
 
-  # preprocess to get all schems, lose the duplication
+  # preprocess to get all schems, always use all colonies
   def collect_data(options)
     capsuleers.each do |capsuleer|
       self.planet_inputs[capsuleer.name] = {}
@@ -60,31 +60,10 @@ class Report
     end.join
   end
 
-  def inputs_by_colony(options={})
-    collect_data(options) # always get all data
-    report_text = ""
-
-    capsuleers.each do |capsuleer|
-      next if options.has_key?(:capsuleer) &&
-        options[:capsuleer] != capsuleer.name
-      report_text += "#{capsuleer.name}\n"
-
-      capsuleer.colonies.each do |colony|
-        next if options.has_key?(:planet) &&
-          options[:planet] != colony.name
-        next if options.has_key?(:system) &&
-          !colony.name.upcase.include?(options[:system].upcase)
-        report_text += "#{colony.name}\t#{colony.short_type}\t\n"
-
-        unless planet_inputs[capsuleer.name][colony.name].empty?
-          report_text += inputs_for_capsuleer_colony(capsuleer, colony)
-          report_text += "\n"
-        end
-      end
-
-      report_text += section_break
-    end
-
+  def inputs_by_colony_for_capsuleer_colony(capsuleer, colony)
+    report_text = "#{colony.name}\t#{colony.short_type}\t\n"
+    report_text += inputs_for_capsuleer_colony(capsuleer, colony)
+    report_text += "\n"
     report_text
   end
 
@@ -113,45 +92,81 @@ class Report
       tabs.times do
         report_text += "\t"
       end
-      report_text +="\t#{count}:#{outputs[name]} #{(count.to_f/outputs[name].to_f).round(2)}\n"
+      report_text +=
+        "\t#{count}:#{outputs[name]} " +
+        "#{(count.to_f/outputs[name].to_f).round(2)}\n"
     end
     report_text
   end
 
-  def products_by_colony(options={})
-    collect_data(options) # always get all data
+  def planet_schematic(pin)
+    planet_schematic = Entity::PlanetSchematic.
+      retrieve(pin, eve_db)
+    "#{planet_schematic.name} (#{pin})\n"
+  end
+
+  def products_for_capsuleer_colony(capsuleer, colony)
+    report_text = "#{colony.name}\t#{colony.short_type}\t"
+
+    colony.unique_schematics.each_with_index do |pin,i|
+      report_text += "\t\t\t" if i > 0
+      report_text += planet_schematic(pin)
+    end
+
+    unless planet_inputs[capsuleer.name][colony.name].empty?
+      report_text += "Inputs:\n"
+      report_text +=
+        inputs_for_capsuleer_colony(capsuleer, colony)
+      report_text += "\n"
+    end
+
+    report_text
+  end
+
+  def capsuleer_colonies(options)
     report_text = ""
 
     capsuleers.each do |capsuleer|
       next if options.has_key?(:capsuleer) &&
         options[:capsuleer] != capsuleer.name
+
       report_text += "#{capsuleer.name}\n"
-      report_text += "Colony\t\tType\tProducts\n"
+      unless options.has_key?(:inputs_only)
+        report_text += "Colony\t\tType\tProducts\n"
+      end
+
       capsuleer.colonies.each do |colony|
         next if options.has_key?(:planet) &&
           options[:planet] != colony.name
         next if options.has_key?(:system) &&
           !colony.name.upcase.include?(options[:system].upcase)
-        report_text += "#{colony.name}\t#{colony.short_type}\t"
 
-        colony.unique_schematics.each_with_index do |pin,i|
-          report_text += "\t\t\t" if i > 0
-
-          # TODO relate schem to pins/colonies
-          planet_schematic = Entity::PlanetSchematic.retrieve(pin, eve_db)
-          report_text += "#{planet_schematic.name} (#{pin})\n"
-        end
-
-        unless planet_inputs[capsuleer.name][colony.name].empty?
-          report_text += "Inputs:\n"
-          report_text += inputs_for_capsuleer_colony(capsuleer, colony)
-          report_text += "\n"
-        end
+        result = yield(capsuleer, colony)
+        report_text += result unless result.blank?
       end
 
       report_text += section_break
     end
 
     report_text
+  end
+
+
+  def inputs_by_colony(options={})
+    collect_data(options)
+
+    capsuleer_colonies(options) do |capsuleer, colony|
+      unless planet_inputs[capsuleer.name][colony.name].empty?
+        inputs_by_colony_for_capsuleer_colony(capsuleer, colony)
+      end
+    end
+  end
+
+  def products_by_colony(options={})
+    collect_data(options)
+
+    capsuleer_colonies(options) do |capsuleer, colony|
+      products_for_capsuleer_colony(capsuleer, colony)
+    end
   end
 end
